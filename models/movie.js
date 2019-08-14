27a8fae83562c021f856242622f19a23b7aa460e
Greg <gmerrill03@gmail.com>
2019-08-14 15:43:28 -0700

    add finished mongoose-movies

--- models/movie.js
+++ models/movie.js
@@ -2,9 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
-    title: String,
-    releaseYear: Number,
-    mpaaRating: String,
-    cast: [String]
+    title: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        default: function() {
+            return new Date().getFullYear();
+        },
+        min: 1927
+    },
+    mpaaRating: {
+        type: String,
+        enum: ['G', 'PG', 'PG-13', 'R']
+    },
+    cast: [String],
+    nowShowing: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
 });
 
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Movie', movieSchema);
